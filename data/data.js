f51c897f2a8d0de75b29e616d8b7a5fb9d8ec343
NULL40 <ananymousman5@gmail.com>
2023-10-17 14:50:22 +0530

    first commit

---
+++ data/data.js
@@ -0,0 +1,13 @@
+export const cardsData = [
+    {
+      imgSrc: 'images/Wellness and Relaxation Therapies aurveda.jpg',
+      title: 'Wellness and Relaxation Therapies',
+      description: 'Tiredness and body pain are two of the most common scenarios occurring at the end of a long day...',
+    },
+    {
+      imgSrc: 'images/head shoulder neck and foot massage.jpg',
+      title: 'Head, shoulder neck, and foot massage.',
+      description: 'We provide quality techniques of massage to ensure betterment and relaxation for those who are exhausted.',
+    },
+    // Add more card objects as needed
+  ];
